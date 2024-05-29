6b2bd68e19bbccee2571e71159850a9fe457453d
Tabaro Christian <tabarochristian@yahoo.com>
2024-05-29 11:23:25 +0200

    docker

--- docker/backend/server-entrypoint.sh
+++ docker/backend/server-entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+# Wait for the server volume to be available
 until cd /app/backend
 do
     echo "Waiting for server volume..."
 done
 
+# Wait for the database to be ready and apply migrations
 until python manage.py makemigrations
 do
     echo "Waiting for db to be ready..."
@@ -17,12 +19,14 @@ do
     sleep 2
 done
 
-#until python manage.py createsuperuser --noinput --email info@sycamore.cd
-#do
-#    echo "Waiting for superuser to be created ..."
-#done
+# Uncomment if you need to create a superuser automatically
+# until python manage.py createsuperuser --noinput --email info@sycamore.cd
+# do
+#     echo "Waiting for superuser to be created..."
+# done
 
+# Uncomment to collect static files (usually needed for production)
 # python manage.py collectstatic --noinput
-# gunicorn payday.wsgi:application --bind 0.0.0.0:8000 --autoscale=4,1
 
-gunicorn payday.wsgi --bind 0.0.0.0:8000
+# Start the Gunicorn server
+exec gunicorn payday.wsgi:application --bind 0.0.0.0:8000
--- docker/backend/worker-entrypoint.sh
+++ docker/backend/worker-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+# Wait for the server volume to be available
 until cd /app/backend
 do
     echo "Waiting for server volume..."
 done
 
-# run a worker :)
-celery -A payday worker -l info -c 4 --beat -E
+# Start the Celery worker with beat
+exec celery -A payday worker -l info -c 4 --beat -E
