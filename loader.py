5e858d2a1fec3e533f919f89caf42f89d673c112
Tabaro Christian <tabarochristian@yahoo.com>
2024-06-08 10:09:20 +0100

    update

--- loader.py
+++
@@ -1,7 +0,0 @@
-from django.db import transaction
-from django.core.management import call_command
-
-with transaction.atomic(using='default'):
-    print("Starting...")
-    call_command('loaddata', 'data.json', database='default')
-    print("Done.")
