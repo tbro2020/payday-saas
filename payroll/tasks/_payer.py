e38a1ae641b2f56ca7e4d3fd2a9a3182f54b6875
Tabaro Christian <tabarochristian@yahoo.com>
2024-06-18 23:29:35 +0100

    Update _payer.py

--- payroll/tasks/_payer.py
+++ payroll/tasks/_payer.py
@@ -186,21 +186,23 @@ class Payer(Task):
             item_paid_codes = set(item_paid_queryset.values_list('code', flat=True))
 
         for item in items:
-            print(item.code, employee.registration_number)
-            if item.code in item_paid_codes: continue
-            if not eval(item.condition, locals()): continue
-            time, qpe, qpp = self.evaluate_formulas(item, employee, payslip)
-            if int(qpe) == 0 and int(qpp) == 0: continue
-            item_to_pay_queryset.append(ItemPaid(
-                code=item.code,
-                type_of_item=item.type_of_item,
-                name=item.name, time=time, rate=round(qpe/time, 2) if time else 0,
-                amount_qp_employer=qpp, amount_qp_employee=qpe,
-                taxable_amount=qpe if getattr(item, 'is_taxable', False) else 0,
-                social_security_amount=qpe if getattr(item, 'is_social_security', False) else 0,
-                is_bonus=getattr(item, 'is_bonus', False), is_payable=getattr(item, 'is_payable', True),
-                payslip=payslip, created_by=self.payroll.created_by
-            ))
+            try:
+                if item.code in item_paid_codes: continue
+                if not eval(item.condition, locals()): continue
+                time, qpe, qpp = self.evaluate_formulas(item, employee, payslip)
+                if int(qpe) == 0 and int(qpp) == 0: continue
+                item_to_pay_queryset.append(ItemPaid(
+                    code=item.code,
+                    type_of_item=item.type_of_item,
+                    name=item.name, time=time, rate=round(qpe/time, 2) if time else 0,
+                    amount_qp_employer=qpp, amount_qp_employee=qpe,
+                    taxable_amount=qpe if getattr(item, 'is_taxable', False) else 0,
+                    social_security_amount=qpe if getattr(item, 'is_social_security', False) else 0,
+                    is_bonus=getattr(item, 'is_bonus', False), is_payable=getattr(item, 'is_payable', True),
+                    payslip=payslip, created_by=self.payroll.created_by
+                ))
+            except Exception as ex:
+                print(ex)
         return ItemPaid.objects.bulk_create(item_to_pay_queryset)
 
     def insert_items_from_df(self, df, payslip, employee):
@@ -212,7 +214,7 @@ class Payer(Task):
         df['est payable'] = df['est une prime'].map({'TRUE': True, 'FALSE': False})
 
         df.drop('matricule', axis=1, inplace=True)
-        df['created_by'] = payslip.created_by.pk
+        # df['created_by'] = payslip.created_by.pk
         df['payslip'] = payslip.pk
 
         columns = {
