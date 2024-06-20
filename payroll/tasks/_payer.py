0b9b3a6fca216fc08e800aebfbb49898ccdd81e7
Tabaro Christian <tabarochristian@yahoo.com>
2024-06-21 00:21:13 +0100

    Update _payer.py

--- payroll/tasks/_payer.py
+++ payroll/tasks/_payer.py
@@ -308,8 +308,12 @@ class Payer(Task):
         taxable_amount *= tranche['percentage']
         taxable_amount += 4860
 
+        print(taxable_amount)
+
         bonus = items.filter(is_bonus=True).aggregate(amount=Sum('taxable_amount'))['amount'] or 0
         bonus = bonus*0.03
+
+        print(bonus)
 
         taxable_amount = taxable_amount + bonus
 
