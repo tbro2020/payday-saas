37bc7207cc2866ef2d94c2e53e0911b9e584a123
Tabaro Christian <tabarochristian@yahoo.com>
2024-07-02 15:26:19 +0100

    Update synthesis_by_employee.py

--- payroll/views/synthesis_by_employee.py
+++ payroll/views/synthesis_by_employee.py
@@ -65,7 +65,7 @@ class SynthesisByEmployee(BaseView):
             'CADRE DIRECTION': 'DIRIGEANTS'
         }, inplace=True)
         
-        df.loc[:,['name', 'DIRIGEANTS', 'CADRE', 'MAITRISE', 'EXECUTANT']]
+        df = df[['name', 'DIRIGEANTS', 'CADRE', 'MAITRISE', 'EXECUTANT']]
 
         # Flatten the columns: Convert multi-index to a single level
         df.columns.name = None  # Remove column index name if it exists
