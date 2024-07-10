0409f3a1e33a5a4607f1a1ee3deb21610ad8dd64
Tabaro Christian <tabarochristian@yahoo.com>
2024-07-10 15:41:19 +0100

    Update filters.py

--- payroll/filters.py
+++ payroll/filters.py
@@ -14,21 +14,21 @@ class PayslipFilter(FilterSet):
     grade = ChoiceFilter(label=_('Grade'), method='by_grade', choices=Grade.objects.values_list('name', 'name').distinct(), widget=forms.Select(attrs={'class': 'form-control'}))
 
     def search(self, queryset, name, value):
-        fields = ['_employee__registration_number', '_employee__middle_name', '_employee__last_name']
+        fields = ['employee__registration_number', 'employee__middle_name', 'employee__last_name']
         query = reduce(lambda q, field: q | Q(**{f"{field}__icontains": value}), fields, Q())
         return queryset.filter(query).distinct()
 
     def by_branch(self, queryset, name, value):
-        return queryset.filter(_employee__branch__name=value)
+        return queryset.filter(employee__branch__name=value)
     
     def by_grade(self, queryset, name, value):
-        return queryset.filter(_employee__grade__name=value)
+        return queryset.filter(employee__grade__name=value)
 
     def by_payer(self, queryset, name, value):
         return queryset.filter(payer__name=value)
     
     def by_status(self, queryset, name, value):
-        return queryset.filter(_employee__status__name=value)
+        return queryset.filter(employee__status__name=value)
 
     class Meta:
         fields = ('branch', 'grade', 'payer', 'status')
