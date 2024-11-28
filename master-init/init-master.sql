873c3731fc4b5c060f733ed0124b6a2b42102a2a
Tabaro Christian <tabarochristian@yahoo.com>
2024-11-28 20:02:23 +0800

    Update init-master.sql

--- master-init/init-master.sql
+++ master-init/init-master.sql
@@ -10,10 +10,13 @@ END $$;
 
 -- PostgreSQL settings for replication
 ALTER SYSTEM SET listen_addresses = '*';
-ALTER SYSTEM SET wal_level = logical;
-ALTER SYSTEM SET max_wal_senders = 4;
-ALTER SYSTEM SET max_replication_slots = 4;
+ALTER SYSTEM SET wal_level = 'logical';
+ALTER SYSTEM SET max_wal_senders = 10;
+ALTER SYSTEM SET max_replication_slots = 10;
 ALTER SYSTEM SET max_wal_size = '2GB';
+
+-- Create replication slot
+SELECT pg_create_physical_replication_slot('replication_slot');
 
 -- Reload PostgreSQL configuration
 SELECT pg_reload_conf();
