6e281bdc105e5a3f6a7a2e55769280d0d1adb87f
Tabaro Christian <tabarochristian@yahoo.com>
2024-11-28 21:57:53 +0800

    revised

--- slave-init/init-slave.sql
+++
@@ -1,9 +0,0 @@
--- Configure the slave to connect to the master
-ALTER SYSTEM SET primary_conninfo = 'host=master port=5432 user=payday password=payday sslmode=prefer';
-ALTER SYSTEM SET primary_slot_name = 'replication_slot';
-
--- Place the standby.signal file
-\! cp /tmp/standby.signal /var/lib/postgresql/data/standby.signal
-
--- Reload PostgreSQL configuration
-SELECT pg_reload_conf();
