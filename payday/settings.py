d209a5aebcf8f428b46197f6cb9379887ec8fb14
Tabaro Christian <tabarochristian@yahoo.com>
2024-12-13 16:51:43 +0800

    i18n

--- payday/settings.py
+++ payday/settings.py
@@ -191,7 +191,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "fr"
+LANGUAGE_CODE = "en"
 
 TIME_ZONE = "UTC"
 
@@ -199,6 +199,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = [BASE_DIR / 'locale']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
